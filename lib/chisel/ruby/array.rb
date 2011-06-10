080aba55c8b7af5946fb4e006073b475c51b33ae
Rockwell Schrock <schrockwell@gmail.com>
2011-06-10 00:06:33 -0400

    Added command-line option support
    Added optional WEBrick server hosted at output directory
    
    Began adding file system watcher support but reverted; needs some more thought before implementation
    
    Did not implement --verbose yet

--- lib/chisel/ruby/array.rb
+++ lib/chisel/ruby/array.rb
@@ -1,5 +1,5 @@
 class Array
-	def sort_by(method_name, ascending=true)
+	def sort_by_method(method_name, ascending=true)
 		method_name = method_name.to_sym
 		self.sort do |left, right|
 			if left.respond_to?(method_name) and right.respond_to?(method_name)
